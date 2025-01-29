d5d6ac117f610c762a6a74cf260dc52b7f88eece
martinnacimiento <martinnacimiento98@gmail.com>
2025-01-29 01:47:00 -0300

    feat: add values example

---
+++ values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println("Martin" + " " + "Nacimiento")
+	fmt.Println("1+1 = ", 1 + 1)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
