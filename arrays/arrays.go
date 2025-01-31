afd324dedcb44a474d44b9f026abdeb4043dd1d1
martinnacimiento <martinnacimiento98@gmail.com>
2025-01-31 01:20:48 -0300

    feat: add arrays example

---
+++ arrays/arrays.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len: ", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	c := [...]int{10, 8: 40, 50}
+	fmt.Println("idx:", c)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
+	threeD := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	fmt.Println("2d: ", threeD)
+}
